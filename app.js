a935f553ee8437a65d54faca986753f2f95cc7d1
0xCipher <64740712+0xCipher-max@users.noreply.github.com>
2021-06-05 19:51:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path"); 
+const app = express();
+const port = 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('index.pug', params);
+})
+app.get('/about', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('about.pug', params);
+})
+app.get('/services', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('services.pug', params);
+})
+app.get('/ContactUs', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('contact_us.pug', params);
+})
+app.get('/Classinfo', (req, res)=>{ 
+    const params = { }
+    res.status(200).render('class_info.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
